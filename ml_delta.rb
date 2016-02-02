e90c32b634b58c635e0e84a42351e0264a3c78db
Eddie Kao <eddie@digik.com.tw>
2016-02-02 09:35:44 +0800

    ruby-alize

--- ml_delta.rb
+++ ml_delta.rb
@@ -6,12 +6,7 @@ DEFAULT_RANDOM_MIN = -0.5
 
 class MLDelta
   @@sharedDelta = MLDelta.new
-  attr_accessor :patterns
-  attr_accessor :weights
-  attr_accessor :targets
-  attr_accessor :learningRate
-  attr_accessor :maxIteration
-  attr_accessor :convergenceValue
+  attr_accessor :learning_rate, :max_iteration, :convergence_value
   attr_accessor :activeMethod
 
   attr_accessor :iterationBlock
@@ -21,13 +16,12 @@ class MLDelta
     @active_function = MLActiveFunction.new
     @iteration       = 0
     @sum_error        = 0.0
-
     @patterns         = []
     @weights          = []
     @targets          = []
-    @learningRate     = 0.5
-    @maxIteration     = 1
-    @convergenceValue = 0.001
+    @learning_rate     = 0.5
+    @max_iteration     = 1
+    @convergence_value = 0.001
     @activeMethod     = MLActiveMethod::TANH
 
     @iterationBlock   = nil
@@ -41,28 +35,26 @@ class MLDelta
     return @@sharedDelta
   end
 
-  def addPatterns(inputs, target)
+  def add_patterns(inputs, target)
     @patterns << inputs
     @targets << target
   end
 
-  def setupWeights(weights)
+  def setup_weights(weights)
     @weights.clear if @weights.count > 0
     @weights += weights
   end
 
   def randomWeights
-    if @weights.count > 0
-      @weights.clear()
-    end
+    @weights.clear if @weights.count > 0
 
     # Follows the inputs count to decide how many weights it needs.
-    _randomMaker   = Random.new
-    _inputNetCount = @patterns.first().count
-    _inputMax      = DEFAULT_RANDOM_MAX / _inputNetCount
-    _inputMin      = DEFAULT_RANDOM_MIN / _inputNetCount
+    randomMaker = Random.new
+    input_net_count = @patterns.first().count
+    input_max = DEFAULT_RANDOM_MAX / input_net_count
+    input_min = DEFAULT_RANDOM_MIN / input_net_count
     for i in (0..._inputNetCount)
-      @weights.push(_randomMaker.rand(_inputMin.._inputMax))
+      @weights.push(randomMaker.rand(input_min..input_max))
     end
   end
 
@@ -71,7 +63,7 @@ class MLDelta
     @sum_error = 0.0
     @patterns.each_with_index{ |inputs, patternIndex| _turningWeightsWithInputs(inputs, @targets[patternIndex]) }
 
-    if (@iteration >= @maxIteration) || (_calculateIterationError() <= @convergenceValue)
+    if (@iteration >= @max_iteration) || (_calculateIterationError() <= @convergence_value)
       @completionBlock.call( true, @weights, @iteration ) unless @completionBlock.nil?
     else
       @iterationBlock.call( @iteration, @weights ) unless @iterationBlock.nil?
@@ -79,18 +71,18 @@ class MLDelta
     end
   end
 
-  def trainingWithCompletion(&_block)
-    @completionBlock = _block
+  def training_with_completion(&block)
+    @completionBlock = block
     training
   end
 
-  def trainingWithIteration(iterationBlock, completionBlock)
+  def training_with_iteration(iterationBlock, completionBlock)
     @iterationBlock = iterationBlock
     @completionBlock = completionBlock
     training
   end
 
-  def directOutputByPatterns(inputs, &block)
+  def direct_output_by_patterns(inputs, &block)
     block.call(_fOfNetWithInputs(inputs)) if block_given?
   end
 
@@ -99,31 +91,31 @@ class MLDelta
     matrix.map{ |obj| obj * number }
   end
 
-  def _plusMatrix(_matrix, _anotherMatrix)
-    _matrix.collect.with_index{ |obj, i| obj + _anotherMatrix[i] }
+  def plus_matrix(matrix, anotherMatrix)
+    matrix.collect.with_index{ |obj, i| obj + anotherMatrix[i] }
   end
 
   def _activateOutputValue(net_output)
     case @activeMethod
       when MLActiveMethod::SGN
-        @active_function.sgn(net_output)
+        @active_function.sgn net_output
       when MLActiveMethod::SIGMOID
-        @active_function.sigmoid(net_output)
+        @active_function.sigmoid net_output
       when MLActiveMethod::TANH
-        @active_function.tanh(net_output)
+        @active_function.tanh net_output
       when MLActiveMethod::RBF
-        @active_function.rbf(net_output, 2.0)
+        @active_function.rbf net_output, 2.0
       else
         # Nothing else
         net_output
     end
   end
 
-  def _fOfNetWithInputs(_inputs)
+  def _fOfNetWithInputs(inputs)
     # to_f
-    _sum = 0.0
-    _inputs.each.with_index{ |obj, i| _sum += ( obj * @weights[i] ) }
-    return _activateOutputValue(_sum)
+    sum = 0.0
+    inputs.each.with_index{ |obj, i| sum += ( obj * @weights[i] ) }
+    _activateOutputValue(sum)
   end
 
   def _fDashOfNet(net_output)
@@ -135,7 +127,7 @@ class MLDelta
       when MLActiveMethod::TANH
         @active_function.dashTanh(net_output)
       when MLActiveMethod::RBF
-        #_@active_function.dashRbf(net_output)
+        #@active_function.dashRbf(net_output)
       else
         # Nothing else
         net_output
@@ -152,32 +144,29 @@ class MLDelta
   end
 
   def _turningWeightsWithInputs(inputs, target_value)
-    _weights      = @weights
-    _netOutput    = _fOfNetWithInputs(inputs)
-    _dashOutput   = _fDashOfNet(_netOutput)
-    _errorValue   = target_value - _netOutput
+    net_output = _fOfNetWithInputs(inputs)
+    dash_output = _fDashOfNet(net_output)
+    error_value = target_value - net_output
 
     # new weights = learning rate * (target value - net output) * f'(net) * x1 + w1
-    _sigmaValue   = @learningRate * _errorValue * _dashOutput
-    _deltaWeights = multiply_matrix(inputs, _sigmaValue)
-    _newWeights   = _plusMatrix(_weights, _deltaWeights)
+    simga_value = @learning_rate * error_value * dash_output
+    delta_weights = multiply_matrix(inputs, simga_value)
+    new_weights = plus_matrix(@weights, delta_weights)
 
-    @weights.clear()
-    @weights += _newWeights
-    sum_error(_errorValue)
+    setup_weights(new_weights)
+    sum_error(error_value)
   end
-
 end
 
 # Use methods
 delta                  = MLDelta.new
 delta.activeMethod     = MLActiveMethod::TANH
-delta.learningRate     = 0.8
-delta.convergenceValue = 0.001
-delta.maxIteration     = 1000
-delta.addPatterns([1.0, -2.0, 0.0, -1.0], -1.0)
-delta.addPatterns([0.0, 1.5, -0.5, -1.0], 1.0)
-delta.setupWeights([1.0, -1.0, 0.0, 0.5])
+delta.learning_rate     = 0.8
+delta.convergence_value = 0.001
+delta.max_iteration     = 1000
+delta.add_patterns([1.0, -2.0, 0.0, -1.0], -1.0)
+delta.add_patterns([0.0, 1.5, -0.5, -1.0], 1.0)
+delta.setup_weights([1.0, -1.0, 0.0, 0.5])
 #delta.randomWeights()
 
 iterationBlock = Proc.new do |iteration, weights|
@@ -186,12 +175,12 @@ end
 
 completionBlock = Proc.new do |success, weights, totalIteration|
   puts "success : #{success}, weights : #{weights}, totalIteration : #{totalIteration}"
-  delta.directOutputByPatterns([1.0, -2.0, 0.0, -1.0]){ |predication| puts "predication result is #{predication}" }
+  delta.direct_output_by_patterns([1.0, -2.0, 0.0, -1.0]){ |predication| puts "predication result is #{predication}" }
 end
 
-delta.trainingWithIteration(iterationBlock, completionBlock)
+delta.training_with_iteration(iterationBlock, completionBlock)
 
-# delta.trainingWithCompletion {
+# delta.training_with_completion {
 #   |success, weights, totalIteration|
 #   puts "success : #{success}, weights : #{weights}, totalIteration : #{totalIteration}"
 # }
